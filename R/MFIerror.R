279f1c6c0069bd4bc78ab30b73f72b68b953e480
Stefan Roediger <stefan_roediger@gmx.de>
2013-05-26 00:00:00 +0000

    version 0.0.2

--- R/MFIerror.R
+++ R/MFIerror.R
@@ -1,5 +1,5 @@
-"MFIerror" <-
-function (x, y, CV=FALSE, RSD=FALSE, rob=FALSE, PLOT=TRUE, type="p", pch=19, length=0.05, col="black")
+"MFIerror" <- function(x, y, CV = FALSE, RSD = FALSE, rob = FALSE, errplot = TRUE, 
+			     type = "p", pch = 19, length = 0.05, col = "black")
 {	#Define if "robust" or standard function should be used as measures
 	options(warn = -1)
 	#Test if x and y exist.
@@ -14,7 +14,7 @@ function (x, y, CV=FALSE, RSD=FALSE, rob=FALSE, PLOT=TRUE, type="p", pch=19, len
 			} else {
 				loc.fct <- mean
 				dev.fct <- sd
-				}
+			}
 	y.m	<-	apply(y, 1, loc.fct)
 	y.sd	<-	apply(y, 1, dev.fct)
 	if (RSD) {
@@ -22,18 +22,23 @@ function (x, y, CV=FALSE, RSD=FALSE, rob=FALSE, PLOT=TRUE, type="p", pch=19, len
 	   
 		  } else {
 			  y.cv	<-	y.sd / y.m
-			    }
+		  }
 	res	<-	data.frame(x, y.m, y.sd, y.cv)
-	names(res)	<- c("Temperature", "Location", "Deviation", "Coefficient of Variance")
+
+	if (rob == TRUE && RSD == FALSE) {names(res)	<- c("Temperature", "Location (Median)", "Deviation (MAD)", "Coefficient of Variance (RSD [%])")}
+	if (rob == FALSE && RSD == FALSE) {names(res)	<- c("Temperature", "Location (Mean)", "Deviation (SD)", "Coefficient of Variance (RSD [%])")}
+	if (rob == TRUE && RSD == TRUE) {names(res)	<- c("Temperature", "Location (Median)", "Deviation (MAD)", "Coefficient of Variance (RSD)")}
+	if (rob == FALSE && RSD == TRUE) {names(res)	<- c("Temperature", "Location (Mean)", "Deviation (SD)", "Coefficient of Variance (RSD)")}
+
 	#Plot the Coefficient of Variance
-	if (PLOT) {
+	if (errplot) {
 		   if (CV) {
-			    plot(res[,1], res[,4], xlab="T", ylab="CV", col=col, pch=pch)
+			    plot(res[,1], res[,4], xlab = "T", ylab = "CV", col = col, pch = pch)
 		      #Plot the location with error bars.
 		      } else {
-			      plot(res[,1], res[,2], ylim=c(min(res[,2] - res[,3]), max(res[,2] + res[,3])), xlab="T", ylab="MFI", type=type)
-			      arrows(res[,1], res[,2] + res[,3], res[,1], res[,2] - res[,3], angle=90, code=3, length=length, col=col)
-			      }
+			      plot(res[,1], res[,2], ylim = c(min(res[,2] - res[,3]), max(res[,2] + res[,3])), xlab = "T", ylab = "MFI", type = type)
+			      arrows(res[,1], res[,2] + res[,3], res[,1], res[,2] - res[,3], angle = 90, code = 3, length = length, col = col)
+		      }
 		  }
 	#res is the an object of the type data.frame containing the temperature, location, deviation and coefficient of variance.
 	return(res = res)
--- R/diffQ2.R
+++ R/diffQ2.R
@@ -1,48 +1,75 @@
-"diffQ2" <-
-function (xy,FCT=max,fws=8,col=2,PEAK=FALSE,deriv=FALSE,negDeriv=TRUE,derivlimits=FALSE,derivlimitsline=FALSE,vertiline=FALSE)
+"diffQ2" <- function(xy, fct = max, fws = 8, col = 2, plot = FALSE, verbose = FALSE, 
+			 peak = FALSE, deriv = FALSE, negderiv = TRUE, derivlimits = FALSE, 
+			 derivlimitsline = FALSE, vertiline = FALSE, rsm = FALSE)
 {
-	    #Test if fws (number of neighbors) is within a meaningful range.
+	    # Test if fws (number of neighbors) is within a meaningful range.
 	    options(warn = -1)
 	      fws <- round(fws)
-	      if (fws < 1 || fws > 8) 
-	      stop("Fit window size must be within 1 and 8.")
+	      if (fws < 2 || fws > 8) 
+	      stop("Fit window size must be within 2 and 8.")
 	    
-	    #Calls instances of diffQ to calculate the Tm of the first and the second derivatives.
-	    #The arguments are similar to diffQ
-	    #Calculates the first derivative and provides the starting information for the second derivative.
-	    TmD1	<-	diffQ(xy,fws=8,col=2,PEAK=PEAK,deriv=deriv,negDeriv=negDeriv,derivlimits=derivlimits,derivlimitsline=derivlimitsline,vertiline=vertiline)
-	    #Calculates the second derivative and from the first derivative for the minimum of the melting curve.
-	    Tm1D2	<-	diffQ(TmD1$xy,FCT=min,fws=fws,col=col,PEAK=PEAK,deriv=deriv,negDeriv=FALSE,derivlimits=derivlimits,derivlimitsline=derivlimitsline,vertiline=vertiline)
-	    #Calculates the second derivative and from the first derivative for the maximum of the melting curve.
-	    Tm2D2	<-	diffQ(TmD1$xy,FCT=max,fws=fws,col=col,PEAK=PEAK,deriv=deriv,negDeriv=FALSE,derivlimits=derivlimits,derivlimitsline=derivlimitsline,vertiline=vertiline)
+	    # Calls instances of diffQ to calculate the Tm of the first and the second derivatives.
+	    # The arguments are similar to diffQ
+	    # Calculates the first derivative and provides the starting information for the second derivative.
+	    TmD1	<-	diffQ(xy, fct = fct, fws = fws, verbose = TRUE, rsm = rsm)
 
-	    DATA	<-	TmD1[["xy"]]
-	    LIMIT	<-	Tm1D2[["limits_xQ"]]
-	    REG1	<- 	subset(DATA, DATA[["xQ"]] >= LIMIT[1] 
-					& DATA[["xQ"]] <= LIMIT[length(LIMIT)], select=c(xQ,diffQ))
+	    if (TmD1[["temperature"]][["mean.T.delta"]] >= 0.5) {
 
-	    LIMIT	<-	Tm2D2[["limits_xQ"]]
-	    REG2	<-	subset(DATA, DATA[["xQ"]] >= LIMIT[1] 
-					& DATA[["xQ"]] <= LIMIT[length(LIMIT)], select=c(xQ,diffQ))
+			tmp.xy <- 	predict(smooth.spline(TmD1$xy[,1], TmD1$xy[,2]), 
+					    seq(min(TmD1$xy[,1]), max(TmD1$xy[,1]), TmD1[["temperature"]][["mean.T.delta"]] / 1.3))
+			xy.smoothed <- 	data.frame(tmp.xy$x, tmp.xy$y)
+		} else (xy.smoothed <-  data.frame(TmD1$xy[,1], smooth.spline(TmD1$xy[,1], TmD1$xy[,2])$y))
 
-	    xQ 		<- NULL; rm(xQ); 
-
-	    #Calculates the melting temperatures based on a local regression with a quadratic polynomial for the Tm1D2 peak value.
-	    lm2.1	<-	lm(REG1[,2] ~ REG1[,1] + I(REG1[,1]^2))
-	    coeflm2.1	<-	data.frame(lm2.1[1])[c(1:3),]
-	    coeflm2_y.1 <-	coeflm2.1[1] + coeflm2.1[2] * Tm1D2[["Tm"]] + coeflm2.1[3] * Tm1D2[["Tm"]]^2
-
-	    #Calculates the melting temperatures based on a local regression with a quadratic polynomial for the Tm2D2 peak value.
-	    lm2.2	<-	lm(REG2[,2] ~ REG2[,1] + I(REG2[,1]^2))
-	    coeflm2.2	<-	data.frame(lm2.2[1])[c(1:3),]
-	    coeflm2_y.2	<-	coeflm2.2[1] + coeflm2.2[2] * Tm2D2[["Tm"]] + coeflm2.2[3] * Tm2D2[["Tm"]]^2
+	    # Calculates the second derivative and from the first derivative for the minimum of the melting curve.
+	    Tm1D2	<-	diffQ(xy.smoothed, fct = min, fws = fws, verbose =  TRUE, col = col, peak = peak, 
+						   deriv = deriv, negderiv = FALSE, derivlimits = derivlimits, 
+						   derivlimitsline = derivlimitsline, vertiline = vertiline)
+	    # Calculates the second derivative and from the first derivative for the maximum of the melting curve.
+	    Tm2D2	<-	diffQ(xy.smoothed, fct = max, fws = fws, verbose =  TRUE, col = col, peak = peak, 
+						   deriv = deriv, negderiv = FALSE, derivlimits = derivlimits, 
+						   derivlimitsline = derivlimitsline, vertiline = vertiline)
 	    
-	    #Vectors of the two melting temperatures of the second derivative.
+	    # Vectors of the two melting temperatures of the second derivative.
 	    x <- c(Tm1D2[["Tm"]], Tm2D2[["Tm"]])
-	    #Vectors of the two intensities at the melting temperatures of the second derivative.
-	    y <- c(coeflm2_y.1, coeflm2_y.2)
+	    # Vectors of the two intensities at the melting temperatures of the second derivative.
+	    # y <- c(coeflm2.y.1, coeflm2.y.2)
+	    y <- c(Tm1D2[["fluoTm"]], Tm2D2[["fluoTm"]])
 
-	    #Returns an object of the type list containing the data and data.frames from above including the approximate 
-	    #difference quotient values, melting temperatures of the first derivative and the second derivative, intensities and used neighbors.
-	    return(list(TmD1=TmD1, Tm1D2=Tm1D2, Tm2D2=Tm2D2, xTm1.2.D2=x, yTm1.2.D2=y))
+	    # Polynom to fit the area of the calculated Tm
+	    poly.fct.TmD1 <- function(xi) TmD1[["fit"]][[4]][1] + TmD1[["fit"]][[4]][2] * xi + TmD1[["fit"]][[4]][3] * xi^2
+	    poly.fct.Tm1D2 <- function(xi) Tm1D2[["fit"]][[4]][1] + Tm1D2[["fit"]][[4]][2] * xi + Tm1D2[["fit"]][[4]][3] * xi^2
+	    poly.fct.Tm2D2 <- function(xi) Tm2D2[["fit"]][[4]][1] + Tm2D2[["fit"]][[4]][2] * xi + Tm2D2[["fit"]][[4]][3] * xi^2
+
+
+	    if (plot) {
+	    # Plot the first derivative
+		  par(fig = c(0,1,0.475,1))
+		  plot(TmD1$xy, xlab = "T", ylab = "-d(F) / dT", type = "b")
+		    abline(v = (TmD1$Tm), col = "grey", lwd = 1.25)
+		    abline(v = (Tm1D2$Tm), col = "grey")
+		    abline(v = (Tm2D2$Tm), col = "grey")
+		    points(TmD1$limits.xQ, TmD1$limits.diffQ, col = "orange", pch = 19)
+		    points(TmD1$Tm, TmD1$fluoTm, pch = 19, col = 2)
+   		    curve(poly.fct.TmD1, TmD1$limits.xQ[1], TmD1$limits.xQ[length(TmD1$limits.xQ)], col = "red", add = TRUE)
+		    if (derivlimits) {points(TmD1$limits.xQ, TmD1$limits.diffQ, cex = 1, pch = 19, col = col)}
+
+	    # Plot the second derivative
+		  par(fig = c(0,1,0,0.525), new = TRUE)
+		  plot(Tm1D2$xy, xlim = c(min(TmD1$xy[,1]), max(TmD1$xy[,1])), xlab = "T", ylab = "-d^2(F) / dT^2", type = "b")
+		    abline(v = (TmD1$Tm), col = "grey")
+		    points(Tm1D2$limits.xQ, Tm1D2$limits.diffQ, col = "green", pch = 19)
+		    points(Tm2D2$limits.xQ, Tm2D2$limits.diffQ, col = "blue", pch = 1)
+		    points(Tm1D2$Tm, Tm1D2$fluo.x, pch = 19, col = 2)
+		    points(Tm2D2$Tm, Tm2D2$fluo.x, pch = 19, col = 2)
+		    curve(poly.fct.Tm1D2, Tm1D2$limits.xQ[1], Tm1D2$limits.xQ[length(Tm1D2$limits.xQ)], col = "red", add = TRUE)
+		    curve(poly.fct.Tm2D2, Tm2D2$limits.xQ[1], Tm2D2$limits.xQ[length(Tm2D2$limits.xQ)], col = "red", add = TRUE)
+ 	      }
+
+	    # Returns an object of the type list containing the data and data.frames from above including the approximate 
+	    # difference quotient values, melting temperatures of the first derivative and the second derivative, intensities and used neighbors.
+	    if (verbose) {
+		  return(list(TmD1 = TmD1, Tm1D2 = Tm1D2, Tm2D2 = Tm2D2, xTm1.2.D2 = x, 
+		      yTm1.2.D2 = y, temperature = TmD1$temperature))
+		} else (return(list(Tm = TmD1$Tm, fluoTm = TmD1$fluoTm, xTm1.2.D2 = x, yTm1.2.D2 = y)))
+
 }
